edfff93cc9e92108e39f1c19c4dec672bda5b3f2
Eric Myhre <hash@exultant.us>
2015-02-27 10:59:04 -0600

    Outline packages.
    We expect to have many implementations of input and output very early on, and multiple executors soon (though we'll focus initial efforts on just one).  Each implementation will get a subpackage, and these top level packages will hold interfaces and serializable structures that are in common.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ executor/doc.go
@@ -0,0 +1,9 @@
+/*
+	Executor implementations are responsible for machining inputs into outputs.
+
+	Executors are command invocations, typically backed by either containers or VMs.
+
+	Imports: executors import from the `input` and `output` package implementations
+	(never the other way around).
+*/
+package executor
