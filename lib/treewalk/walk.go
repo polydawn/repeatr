d4a404d26669b228aede7a617a8f1c677810f730
Eric Myhre <hash@exultant.us>
2015-03-04 20:32:39 -0600

    Start tree walk library.
    I need pre- and post-order visitors to walk a tree, and I need it for both the filesystem (stdlib walk only does pre-order traversal) and for our hash buckets.
    
    It's almost like.... it's almost like I need generics to solve this correctly.  Fancy that.  That NEVER happens in golang.
    
    Oh, wait, it's been a week since I last ran into a problem like this.  Yep, it was about due time for it to happen again.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ lib/treewalk/walk.go
@@ -0,0 +1,50 @@
+package treewalk
+
+import (
+	"errors"
+)
+
+type Node interface {
+	NextChild() Node
+}
+
+func Walk(root Node, preVisit WalkFunc, postVisit WalkFunc) error {
+	return walk(root, preVisit, postVisit)
+}
+
+type WalkFunc func(node Node) error
+
+/*
+	SkipNode is used as a return value from WalkFuncs to indicate that the node named in the call (and all its children) are to be skipped.
+	It only makes sense to return this from the pre-visit function; it's by definition too late after the post-visit function.
+*/
+var SkipNode = errors.New("skip this node")
+
+/*
+	walk recursively descends tree, calling walkFn.
+*/
+func walk(node Node, preVisit WalkFunc, postVisit WalkFunc) error {
+	err := preVisit(node)
+	if err != nil {
+		if err == SkipNode {
+			return nil
+		}
+		return err
+	}
+
+	for next := node.NextChild(); next != nil; next = node.NextChild() {
+		if err := walk(next, preVisit, postVisit); err != nil {
+			return err
+		}
+	}
+
+	err = postVisit(node)
+	if err != nil {
+		if err == SkipNode {
+			return nil
+		}
+		return err
+	}
+
+	return nil
+}
