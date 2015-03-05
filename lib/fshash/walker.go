89b8f3187a3c2de0521b5e9f17b4e374ce97cfae
Eric Myhre <hash@exultant.us>
2015-03-04 19:56:29 -0600

    Refactor variable names.  Reduce redundant joins.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- lib/fshash/walker.go
+++ lib/fshash/walker.go
@@ -14,14 +14,15 @@ import (
 	"polydawn.net/repeatr/lib/fspatch"
 )
 
-func FillBucket(srcPath, destPath string, bucket Bucket, hasherFactory func() hash.Hash) error {
+func FillBucket(srcBasePath, destBasePath string, bucket Bucket, hasherFactory func() hash.Hash) error {
 	// If copying: Dragons: you can set atime and you can set mtime, but you can't ever set ctime again.
 	// Filesystem APIs are constructed such that it's literally impossible to do an attribute-preserving copy in userland.
-	return filepath.Walk(srcPath, func(path string, info os.FileInfo, err error) error {
-		path = "." + strings.TrimPrefix(path, srcPath)
+	return filepath.Walk(srcBasePath, func(srcPath string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+		relPath := "." + strings.TrimPrefix(srcPath, srcBasePath)
+		destPath := filepath.Join(destBasePath, relPath)
 		mode := info.Mode()
 		switch {
 		case mode&os.ModeDir == os.ModeDir:
@@ -29,15 +30,15 @@ func FillBucket(srcPath, destPath string, bucket Bucket, hasherFactory func() ha
 			if err != nil {
 				return err
 			}
-			hdr.Name = path
+			hdr.Name = relPath
 			hdr.ChangeTime = def.Somewhen
 			hdr.AccessTime = def.Somewhen
-			if destPath != "" {
-				if err := os.Mkdir(filepath.Join(destPath, path), mode&os.ModePerm); err != nil {
+			if destBasePath != "" {
+				if err := os.Mkdir(destPath, mode&os.ModePerm); err != nil {
 					return err
 				}
 				// FIXME: this needs post-order traversal to take useful effect
-				if err := fspatch.UtimesNano(filepath.Join(destPath, path), []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
+				if err := fspatch.UtimesNano(destPath, []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
 					return err
 				}
 			}
@@ -45,21 +46,21 @@ func FillBucket(srcPath, destPath string, bucket Bucket, hasherFactory func() ha
 		case mode&os.ModeSymlink == os.ModeSymlink:
 			var link string
 			var err error
-			if link, err = os.Readlink(path); err != nil {
+			if link, err = os.Readlink(srcPath); err != nil {
 				return err
 			}
 			hdr, err := tar.FileInfoHeader(info, link)
 			if err != nil {
 				return err
 			}
-			hdr.Name = path
+			hdr.Name = relPath
 			hdr.ChangeTime = def.Somewhen
 			hdr.AccessTime = def.Somewhen
-			if destPath != "" {
-				if err := os.Symlink(filepath.Join(destPath, path), link); err != nil {
+			if destBasePath != "" {
+				if err := os.Symlink(destPath, link); err != nil {
 					return err
 				}
-				if err := fspatch.LUtimesNano(filepath.Join(destPath, path), []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
+				if err := fspatch.LUtimesNano(destPath, []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
 					return err
 				}
 			}
@@ -74,15 +75,15 @@ func FillBucket(srcPath, destPath string, bucket Bucket, hasherFactory func() ha
 			panic(errors.NotImplementedError.New("TODO"))
 		case mode&os.ModeType == 0: // i.e. regular file
 			// copy data into place and accumulate hash
-			src, err := os.OpenFile(filepath.Join(srcPath, path), os.O_RDONLY, 0)
+			src, err := os.OpenFile(srcPath, os.O_RDONLY, 0)
 			if err != nil {
 				return err
 			}
 			defer src.Close()
 			hasher := hasherFactory()
 			var tee io.Writer
-			if destPath != "" {
-				dest, err := os.OpenFile(filepath.Join(destPath, path), os.O_WRONLY|os.O_CREATE|os.O_EXCL, mode&os.ModePerm)
+			if destBasePath != "" {
+				dest, err := os.OpenFile(destPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, mode&os.ModePerm)
 				if err != nil {
 					return err
 				}
@@ -100,11 +101,11 @@ func FillBucket(srcPath, destPath string, bucket Bucket, hasherFactory func() ha
 			if err != nil {
 				return err
 			}
-			hdr.Name = path
+			hdr.Name = relPath
 			hdr.ChangeTime = def.Somewhen
 			hdr.AccessTime = def.Somewhen
-			if destPath != "" {
-				if err := fspatch.UtimesNano(filepath.Join(destPath, path), []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
+			if destBasePath != "" {
+				if err := fspatch.UtimesNano(destPath, []syscall.Timespec{def.SomewhenTimespec, syscall.NsecToTimespec(hdr.ModTime.UnixNano())}); err != nil {
 					return err
 				}
 			}
