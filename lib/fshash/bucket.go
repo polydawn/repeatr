8a940de82446a218c8fcf8dc0a251da1d8b7bbaf
Eric Myhre <hash@exultant.us>
2015-03-05 22:31:54 -0600

    Make fshash.Bucket traversable with treewalk.
    This iterator is certainly a fragile implementation -- continuing to call an iterator from a prior part of the walk will behave strangely -- but it doesn't need to be anything else, and it runs linearly (no backtracks, no re-covering, and minimal extra memory).
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- lib/fshash/bucket.go
+++ lib/fshash/bucket.go
@@ -3,9 +3,12 @@ package fshash
 import (
 	"archive/tar"
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/spacemonkeygo/errors"
 	"polydawn.net/repeatr/def"
+	"polydawn.net/repeatr/lib/treewalk"
 )
 
 type Metadata tar.Header
@@ -58,7 +61,28 @@ func (m Metadata) MarshalBinary() ([]byte, error) {
 */
 type Bucket interface {
 	Record(metadata Metadata, contentHash []byte)
+	Iterator() (rootRecord RecordIterator)
 }
+
+type Record struct {
+	Metadata    Metadata `json:"m"`
+	ContentHash []byte   `json:"h"`
+}
+
+/*
+	RecordIterator is used for walking Bucket contents in hash-ready order.
+	It's specified separately from Record for three reasons: There will be many
+	Record objects, and so they should be small (the iterators tend to require
+	at least another two words of memory); and the Record objects are
+	serializable the same way for all implementations of Bucket (the iterators
+	may work differently depending on how data is heaped in the Bucket impl).
+*/
+type RecordIterator interface {
+	treewalk.Node
+	Record() Record
+}
+
+var InvalidFilesystem *errors.ErrorClass = errors.NewClass("InvalidFilesystem")
 
 /*
 	FileCollision is reported when the same file path is submitted to a `Bucket`
@@ -68,26 +92,61 @@ type Bucket interface {
 	question of which should be sorted first when creating a deterministic
 	hash of the whole tree.)
 */
-var FileCollision *errors.ErrorClass = errors.NewClass("FileCollision")
+var FileCollision *errors.ErrorClass = InvalidFilesystem.NewClass("FileCollision")
+
+/*
+	MissingTree is reported when iteration over a filled bucket encounters
+	a file that has no parent nodes.  I.e., if there's a file path "./a/b",
+	and there's no entries for "./a", it's a MissingTree error.
+*/
+var MissingTree *errors.ErrorClass = InvalidFilesystem.NewClass("MissingTree")
 
 var _ Bucket = &MemoryBucket{}
 
 type MemoryBucket struct {
 	// my kingdom for a red-black tree or other sane sorted map implementation
-	lines []line
+	lines []Record
 }
 
-type line struct {
-	metadata    Metadata
-	contentHash []byte
+func (b *MemoryBucket) Record(metadata Metadata, contentHash []byte) {
+	b.lines = append(b.lines, Record{metadata, contentHash})
 }
 
-type linesByFilepath []line
+func (b *MemoryBucket) Iterator() RecordIterator {
+	sort.Sort(linesByFilepath(b.lines))
+	// TODO: check for rootedness
+	var that int
+	return &memoryBucketIterator{b.lines, 0, &that}
+}
+
+type memoryBucketIterator struct {
+	lines []Record
+	this  int  // pretending a linear structure is a tree is weird.
+	that  *int // this is the last child walked.
+}
+
+func (i *memoryBucketIterator) NextChild() treewalk.Node {
+	// is the next one still a child?
+	next := *i.that + 1
+	if next >= len(i.lines) {
+		return nil
+	}
+	if strings.HasPrefix(i.lines[next].Metadata.Name, i.lines[i.this].Metadata.Name+"/") {
+		*i.that = next
+		// TODO: check for missing trees
+		// TODO: check for repeated names
+		return &memoryBucketIterator{i.lines, *i.that, i.that}
+	}
+	return nil
+}
+
+func (i memoryBucketIterator) Record() Record {
+	return i.lines[i.this]
+}
+
+// for sorting
+type linesByFilepath []Record
 
 func (a linesByFilepath) Len() int           { return len(a) }
 func (a linesByFilepath) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a linesByFilepath) Less(i, j int) bool { return a[i].metadata.Name < a[j].metadata.Name }
-
-func (b *MemoryBucket) Record(metadata Metadata, contentHash []byte) {
-	b.lines = append(b.lines, line{metadata, contentHash})
-}
+func (a linesByFilepath) Less(i, j int) bool { return a[i].Metadata.Name < a[j].Metadata.Name }
--- lib/fshash/walker_test.go
+++ lib/fshash/walker_test.go
@@ -20,6 +20,10 @@ func Test(t *testing.T) {
 			So(err, ShouldBeNil)
 			f.Write([]byte("zyx"))
 			So(f.Close(), ShouldBeNil)
+			f, err = os.OpenFile("src/d", os.O_RDWR|os.O_CREATE, 0755)
+			So(err, ShouldBeNil)
+			f.Write([]byte("jkl"))
+			So(f.Close(), ShouldBeNil)
 
 			Convey("We can walk and fill a bucket", func() {
 				bucket := &MemoryBucket{}
@@ -27,12 +31,13 @@ func Test(t *testing.T) {
 				So(err, ShouldBeNil)
 
 				Convey("Then the bucket contains the file descriptions", func() {
-					So(len(bucket.lines), ShouldEqual, 4)
+					So(len(bucket.lines), ShouldEqual, 5)
 					sort.Sort(linesByFilepath(bucket.lines))
-					So(bucket.lines[0].metadata.Name, ShouldEqual, ".")
-					So(bucket.lines[1].metadata.Name, ShouldEqual, "./a")
-					So(bucket.lines[2].metadata.Name, ShouldEqual, "./b")
-					So(bucket.lines[3].metadata.Name, ShouldEqual, "./b/c")
+					So(bucket.lines[0].Metadata.Name, ShouldEqual, ".")
+					So(bucket.lines[1].Metadata.Name, ShouldEqual, "./a")
+					So(bucket.lines[2].Metadata.Name, ShouldEqual, "./b")
+					So(bucket.lines[3].Metadata.Name, ShouldEqual, "./b/c")
+					So(bucket.lines[4].Metadata.Name, ShouldEqual, "./d")
 				})
 
 				Convey("Doing it again produces identical descriptions", func() {
@@ -40,12 +45,29 @@ func Test(t *testing.T) {
 					err := FillBucket("src", "", bucket2, sha512.New384)
 					So(err, ShouldBeNil)
 
-					So(len(bucket2.lines), ShouldEqual, 4)
+					So(len(bucket2.lines), ShouldEqual, 5)
 					sort.Sort(linesByFilepath(bucket.lines))
 					sort.Sort(linesByFilepath(bucket2.lines))
 					for i := range bucket.lines {
 						So(bucket2.lines[i], ShouldResemble, bucket.lines[i])
 					}
+				})
+
+				Convey("We can walk the bucket and touch all records", func() {
+					root := bucket.Iterator()
+					So(root.Record().Metadata.Name, ShouldEqual, ".")
+					node_a := root.NextChild().(RecordIterator)
+					So(node_a.Record().Metadata.Name, ShouldEqual, "./a")
+					So(node_a.NextChild(), ShouldBeNil)
+					node_b := root.NextChild().(RecordIterator)
+					So(node_b.Record().Metadata.Name, ShouldEqual, "./b")
+					node_c := node_b.NextChild().(RecordIterator)
+					So(node_c.Record().Metadata.Name, ShouldEqual, "./b/c")
+					So(node_c.NextChild(), ShouldBeNil)
+					node_d := root.NextChild().(RecordIterator)
+					So(node_d.Record().Metadata.Name, ShouldEqual, "./d")
+					So(node_d.NextChild(), ShouldBeNil)
+					So(root.NextChild(), ShouldBeNil)
 				})
 			})
 		}),
