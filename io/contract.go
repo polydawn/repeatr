f32cd152405d5d4e72be01ce17c260830ba3a430
Eric Myhre <hash@exultant.us>
2015-04-16 11:58:09 -0500

    Clean the slate.  Round 2.  Orienting everything around the yielding universe.
    Seems to be working a little better.  Might carry it all the way over the line and drop imperative locations as a separate interface, because they're really the exception, but gonna meditate on that a while yet.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/contract.go
+++ io/contract.go
@@ -1,52 +1,15 @@
 package integrity
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"sort"
+
 	"polydawn.net/repeatr/def"
-	tarinput "polydawn.net/repeatr/input/tar2"
 )
 
-// integrity
-// faith
-
-// tricky bits:
-//   - disbatch happens in multiple layers.
-//     - there's a basic one, and there's another one needed the instant you introduce the cache layer.
-//     - prepare the plugin system for this!
-
-// siloType, dataHash, siloLocation, localLocation >-- MATERIALIZER --> chan {}
-
-// siloType, localLocation, siloLocation >-- SLURPER --> chan dataHash
-
-// The major change here is that it's deeply important that the materializer must *yield* a localLocation instead of accept a localLocation:
-//  this makes it possible to have a trusting cacher do the same manuver.
-// Putting something into a final location is always a separate step:
-//  Usually that's going to take the place of a bind mount (and may involve an indirection that does COW).
-//  It *might* take
-
-// FIXME: no, still dumb.  The *bottom* of the universe still has to be an imperative localLocation.
-
-// Yeah, give the cache a placer.
-// The cache can internally plan to share (or be exclusive on) a filesystem area,
-//  and no part of this impacts executors setting up their own cachers with COW semantics appropriate for them.
-
-// ... What about an executor that just wants the truth and no action?
-//  Sorry, impossible.  Or, the cacher can give you an interface for that too,
-//   but most consumers are going to use the `func (c *cacher) Materializer(Placer) func(siloType, dataHash, siloLocation, localLocation) <-chan err` route.
-//    Hell, that can be a `func AdaptMaterializer(Cacher, Placer) Materializer`.  Nothing's likely to change per cacher.
-
-//
-//type Materializer()
-
-// LET'S TRY THIS AGAIN.
-
-// dataHash, localLocation >-- MATERIALIZER{siloType,[]satisfiers} --> chan {}
-
-// localLocation >-- SLURPER{siloType,[]archiveDestinations} --> chan dataHash
-
-// oh, hey.  look: the contents of the middle shit are actually the same shape.
-
-type Materializer func(dataHash string, siloURI string, destPath string, writable bool) <-chan error
+type Materializer func(dataHash string, siloURI string, destPath string) <-chan error
 
 type Slurper func(scanPath string) <-chan SlurpReport
 
@@ -55,87 +18,133 @@ type SlurpReport struct {
 	Err  error
 }
 
+type Haver func(dataHash string, siloURI string) <-chan HaverReport // so... yes, we're almost certainly willing to compromize this to have a destPath param that may or may not be blatantly ignored.
+
+type HaverReport struct {
+	Path string
+	Err  error
+}
+
 type Placer func(srcPath, destPath string, writable bool) <-chan error
 
-// n.b. dat errur handlin goonna be odd: placer errors, are they input or output?  wrap ur placer for conversion?  embarass.  but, work.
+/*
+	Writable inputs get a COW.
+	RO inputs just bind.
+	Outputs (always writable) don't have input, so also can just bind.
 
-func TarMaterializer() Materializer { // this doesn't need to be a factory anymore :/  all the parameters evaporated
-	return func(dataHash string, siloURI string, destPath string, _ bool) <-chan error {
-		return tarinput.New(def.Input{
-			Type: "tar",
-			Hash: dataHash,
-			URI:  siloURI,
-		}).Apply(destPath)
+	Expect most assemblers to be constructed with a Haver and a Placer.
+*/
+type Assembler func([]AssemblyPart)
+
+type AssemblyPart struct {
+	TargetPath string // in the container fs context
+	SourcePath string // datasource which we want to respect
+	Writable   bool
+	// TODO make sure we get an example that sees how this reacts to outputs: not sure we have enough bits here yet
+}
+
+// sortable by target path (which is effectively mountability order)
+type Assembly []AssemblyPart
+
+func (a Assembly) Len() int           { return len(a) }
+func (a Assembly) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Assembly) Less(i, j int) bool { return a[i].TargetPath < a[j].TargetPath }
+
+//
+// coersion stuff
+//
+
+func TmpdirHaver(workPath string, mat Materializer) Haver {
+	return func(dataHash string, siloURI string) <-chan HaverReport {
+		done := make(chan HaverReport)
+		go func() {
+			defer close(done)
+			path, err := ioutil.TempDir(workPath, "")
+			if err != nil {
+				done <- HaverReport{Err: err}
+				return
+			}
+			err = <-mat(dataHash, siloURI, path)
+			if err != nil {
+				done <- HaverReport{Err: err}
+				return
+			}
+			done <- HaverReport{Path: path}
+		}()
+		return done
 	}
 }
 
-func AufsPlacer(workDirPath string) Placer {
-	return func(srcPath, destPath string, _ bool) <-chan error {
-		// use workdir to host a tempdir to use as the "layer" dir
-		return nil // ignore writable, COW it -- use in combo with bindmounter that understands ro
-	}
-}
-
-func BindPlacer(writerIsolator Placer) Placer {
-	return func(srcPath, destPath string, writable bool) <-chan error {
-		if writable {
-			return writerIsolator(srcPath, destPath, writable)
-			// no need to do another bounce of bind; we're already mounting so we can just do it in place
+func CachingHaver(workPath string, mat Materializer) Haver {
+	return func(dataHash string, siloURI string) <-chan HaverReport {
+		permPath := filepath.Join(workPath, "committed", dataHash)
+		_, statErr := os.Stat(permPath)
+		done := make(chan HaverReport)
+		if os.IsNotExist(statErr) {
+			stageBasePath := filepath.Join(workPath, "staging")
+			go func() {
+				defer close(done)
+				report := <-TmpdirHaver(stageBasePath, mat)(dataHash, siloURI)
+				// keep it around.
+				// build more realistic syncs around this later, but posix mv atomicity might actually do enough.
+				err := os.Rename(report.Path, permPath)
+				if err != nil {
+					done <- HaverReport{Err: err}
+					return
+				}
+				done <- HaverReport{Path: permPath}
+			}()
 		} else {
-			// plain bind mount with ro bits
-			return nil
+			done <- HaverReport{Path: permPath}
+			close(done)
 		}
-	}
-
-}
-
-func AdaptMaterializer(c *Cacher, p Placer) Materializer {
-	return func(dataHash string, siloURI string, destPath string, writable bool) <-chan error {
-		cachePath := c.Get(dataHash, siloURI)
-		return p(cachePath, destPath, writable)
+		return done
 	}
 }
 
-type Cacher struct {
-	BasePath string       // working dir
-	filler   Materializer // NOTE you lost the ability to have another layer of disbatcher here, which might be handy if two different types of materializer happen to safely share a hash space (which, jussayin, several *do*)... though, no, that's sane since the get call just accepts a siloLocation... for now...
+type TheAssembler struct {
+	Placer Placer
 }
 
-func (c *Cacher) Get(dataHash string, siloURI string) (path string) { // blocks
-	cachePath := filepath.Join(c.BasePath, dataHash)
-	// todo something with claims and joining waits too
-	fillErr := <-c.filler(dataHash, siloURI, cachePath, false)
-	panic(fillErr)
-	return cachePath
+var _ Assembler = (&TheAssembler{}).Assemble
+
+func (a *TheAssembler) Assemble(mounts []AssemblyPart) {
+	sort.Sort(Assembly(mounts))
+	//	for
 }
+
+//
+// exapmle
+//
 
 func example() {
-	// assemble a caching tar input system, all the way from job config stuff.
-	inSpec := def.Input{
-		Type: "tar",
-		Hash: "2lkf8vsd",
-		URI:  "file://data/supplier",
+	var formula def.Formula
+	var materializer Materializer // this is ignoring dispatcher for now
+	var wantCache bool
+	var workDir string // probably one per executor; whatever
+
+	// we start with materializers but always coerce them into acting like havers,
+	// just so we can have a consistent interface and drop the caching layer transparently.
+	var haver Haver
+
+	// materializers should be draftable into havers... with or without cachers
+	if wantCache {
+		haver = CachingHaver(filepath.Join(workDir, "cache"), materializer)
+	} else {
+		haver = TmpdirHaver(filepath.Join(workDir, "tmp"), materializer)
 	}
 
-	matDispatcher := MaterializerDispatcher{
-		"tar": TarMaterializer(),
+	// start having all filesystems
+	//var []HaverReport
+	for _, input := range formula.Inputs {
+		//func(dataHash string, siloURI string) <-chan HaverReport
+		// do a for loop you fool
+		haver(input.Hash, input.URI)
+		// TODO collect
+	}
+	for _, output := range formula.Outputs {
+		_ = output
 	}
 
-	var mat Materializer = matDispatcher[inSpec.Type]
-
-	mat(inSpec.Hash, inSpec.URI, inSpec.Location /* translated for rootfs, obvs */, true)
-
-	_ = mat
+	// assemble them into the final tree
 }
-
-type MaterializerDispatcher map[string]Materializer
-
-// guess what else
-// guess what else
-
-// the last stage of placer needs to be in a different level of sync than anything else
-// because the executor might need to do some mkdirs in between the last couple placer operations
-// and it most definitely does not want to have a wildly variable and networked wait inbetween thoseS
-
-// bonus points: please remember that this is going to need progress reporting.
-// anyone returning an error chan right now should be returning a progress monitor instead.
