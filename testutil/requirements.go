4540fa234e0f75725b27ce77de5bbe89fd5f8495
Eric Myhre <hash@exultant.us>
2015-04-30 10:51:51 -0500

    Redo requirements as a test decorator.  Works.
    It's not everything I might've wanted in expressiveness, but it solves the problem of multiple nested convey steps for each requirement.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- testutil/requirements.go
+++ testutil/requirements.go
@@ -43,61 +43,33 @@ func Convey_IfSlowTests(items ...interface{}) {
 	}
 }
 
-// want: better explanation to appear around skipping
-// fun facts
-// - goconvey has a reporter interface, but you need to fork them to add one
-// - you can do skips with parameters
-// - you'd have to wrap names manually
-// - there's no transport available for additional flags (i'd like to treat "skip:todo" differently than "skip:needroot")
-
-// also, ideally, in this stuff below, since we can't convert the top node
-// into a skip and still have any powers of speech, it'd be nice to
-// report a skip for every child (depth 1 is fine)... but alas, no real way unless we
-// proxy literally every `Convey` method.  which is sounding better and better...
-// but then that would require us to repeat copious amounts of gls use too, which is just
-// way, way WAY further off the deep end than is even remotely appropriate for this.
-
 /*
-	Like `Convey`, but accepts a set of `ConveyRequirement`s before `Convey`'s
-	usual arguments, and runs tests only if all requirements are satisified.
-
-	If the requirements are not satisfied, prints a single child `Convey` node
-	describing the requirements and a description of what's missing.
+	Decorates a GoConvey test to check a set of `ConveyRequirement`s,
+	returning a dummy test func that skips (with an explanation!) if any
+	of the requirements are unsatisfied; if all is well, it yields
+	the real test function unchanged.
 */
-func ConveyRequires(items ...interface{}) {
-	// discover requirements
-	var nReqs int
-	var reqs []ConveyRequirement
-	for i, it := range items {
-		if req, ok := it.(ConveyRequirement); ok {
-			reqs = append(reqs, req)
-		} else {
-			nReqs = i
-			break
-		}
-	}
+func Requires(action interface{}, requirements ...ConveyRequirement) func(c convey.C) {
+	// examine requirements
 	var widest int
-	for _, req := range reqs {
-		if len(req.name) > widest {
-			widest = len(req.name)
+	for _, req := range requirements {
+		if len(req.Name) > widest {
+			widest = len(req.Name)
 		}
 	}
-	// extract passthrough work descriptions
-	passItems := items[nReqs:]
-	action := passItems[len(passItems)-1]
 	// check requirements
 	var whynot bytes.Buffer
 	var names []string
 	allSat := true
-	for _, req := range reqs {
-		sat := req.predicate()
+	for _, req := range requirements {
+		sat := req.Predicate()
 		allSat = allSat && sat
-		names = append(names, req.name)
-		fmt.Fprintf(&whynot, "requirement %*q: %v\n", widest+2, req.name, sat)
+		names = append(names, req.Name)
+		fmt.Fprintf(&whynot, "requirement %*q: %v\n", widest+2, req.Name, sat)
 	}
 	// act
 	if allSat {
-		passItems[len(passItems)-1] = func(c convey.C) {
+		return func(c convey.C) {
 			// attempted: inserting another convey that makes a single 'true=true' assertion so we see the prereqs and a green check mark.
 			// doesn't work: doing so causes a leaf node, in which everything is run :/ even if skipped, the remaining `So` that aren't
 			// in another block get attached to it, which makes verrry odd reading, and causes an extra repetition of anything
@@ -112,19 +84,17 @@ func ConveyRequires(items ...interface{}) {
 		}
 	} else {
 		title := "Prereqs: " + strings.Join(names, ", ")
-		passItems[len(passItems)-1] = func(c convey.C) {
-			//convey.SkipConvey(title, func() {})
+		return func(c convey.C) {
 			convey.Convey(title, nil)
 			c.Println()
 			c.Print(whynot.String())
 		}
 	}
-	convey.Convey(passItems...)
 }
 
 type ConveyRequirement struct {
-	name      string
-	predicate func() bool
+	Name      string
+	Predicate func() bool
 }
 
 // Require that the tests are not running with the "short" flag enabled.
@@ -144,10 +114,3 @@ var CanNSRequirement = ConveyRequirement{"can namespace", func() bool {
 		return true
 	}
 }}
-
-// another way through all this would just be to make a function that resolves a slice of ConveyRequirements
-// into a skipConvey token, but that's yet again blockaded by an interface that's pretty abruptly limiting
-// the instant you step outside of the DSL: nothing like the `noSpecifier` or `skipConvey` tokens are exposed.
-
-// wait, `nil` action funcs also hit the skip path, right?
-// yeah, that'd work.  it's not really much different than what we ended up doing here... except it's chainable, for some definition of the word.
