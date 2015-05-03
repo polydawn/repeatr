4bb703ffdcbd7ca2dbe81d4314ce034f3c83079c
Eric Myhre <hash@exultant.us>
2015-05-03 17:02:46 -0500

    Refactor assembler test common steps.
    Outline a bunch of more precise subheaddings (mostly still as todos) -- surprise surprise, one big test that lumps together like 8 major behaviors is *really* hard to debug when it fails in some odd environment that isn't your own -- so we're dealing with that asap.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/placer/assembler_test.go
+++ io/placer/assembler_test.go
@@ -12,67 +12,129 @@ import (
 )
 
 func TestCopyingPlacerCompliance(t *testing.T) {
-	CheckPlacementBasics(t, defaultAssembler{Placer: CopyingPlacer}.Assemble)
+	Convey("Copying placers make data appear into place", t, func() {
+		CheckAssemblerGetsDataIntoPlace(defaultAssembler{Placer: CopyingPlacer}.Assemble)
+	})
 }
 
 func TestBindPlacerCompliance(t *testing.T) {
-	CheckPlacementBasics(t, defaultAssembler{Placer: BindPlacer}.Assemble)
+	Convey("Bind placers make data appear into place", t, func() {
+		CheckAssemblerGetsDataIntoPlace(defaultAssembler{Placer: BindPlacer}.Assemble)
+	})
 }
 
 // you probs want to create that assembler with a variety of placers
-func CheckPlacementBasics(t *testing.T, assemblerFn integrity.Assembler) {
-	Convey("Assembly a series of filesystems should produce a union", t,
+func CheckAssemblerGetsDataIntoPlace(assemblerFn integrity.Assembler) {
+	Convey("Assembly with just a root fs works",
+		testutil.Requires(
+			testutil.RequiresRoot,
+			testutil.WithTmpdir(func() {
+				filefixture.Alpha.Create("./material/alpha")
+				assembleAndScan(
+					assemblerFn,
+					[]integrity.AssemblyPart{
+						{TargetPath: "/", SourcePath: "./material/alpha"},
+					},
+					filefixture.Alpha,
+				)
+			}),
+		),
+	)
+
+	Convey("Assembly with one placement into an existing dir works",
 		testutil.Requires(
 			testutil.RequiresRoot,
 			testutil.WithTmpdir(func() {
 				filefixture.Alpha.Create("./material/alpha")
 				filefixture.Beta.Create("./material/beta")
-				// We're going to try a bunch of tricky things at once:
-				// - placement basics, of course
-				// - one placement inside another, with a directory that already exists
-				// - one placement inside another, with a directory that *doesn't* already exist
-				// - reusing one data source in several active locations
-				// TODO coverage:
-				// - failure path: placement that overlaps a file somewhere
-				// - everything about changes and ensuring they're isolated... deserves a whole battery
-
-				Convey("Assembly should not blow up", FailureContinues, func() {
-					var assembly integrity.Assembly
-					So(func() {
-						assembly = assemblerFn("./assembled", []integrity.AssemblyPart{
-							{TargetPath: "/", SourcePath: "./material/alpha"},
-							{TargetPath: "/a", SourcePath: "./material/beta"},
-							{TargetPath: "/d/d/d", SourcePath: "./material/beta"},
-						})
-					}, ShouldNotPanic)
-
-					Convey("Filesystem should scan as the expected union", func() {
-						scan := filefixture.Scan("./assembled")
-						So(scan.Describe(filefixture.CompareDefaults), ShouldEqual,
-							filefixture.Fixture{Files: []filefixture.FixtureFile{
-								{fs.Metadata{Name: ".", Mode: 0755, ModTime: time.Unix(1000, 2000)}, nil}, // even though the basedir was made by the assembler, this should have the rootfs's properties overlayed onto it
-								{fs.Metadata{Name: "./a"}, nil},                                           // this one's mode and times should be overlayed by the second mount
-								{fs.Metadata{Name: "./a/1"}, []byte{}},
-								{fs.Metadata{Name: "./a/2"}, []byte{}},
-								{fs.Metadata{Name: "./a/3"}, []byte{}},
-								{fs.Metadata{Name: "./b", Mode: 0750, ModTime: time.Unix(5000, 2000)}, nil},
-								{fs.Metadata{Name: "./b/c", Mode: 0664, ModTime: time.Unix(7000, 2000)}, []byte("zyx")},
-								{fs.Metadata{Name: "./d", Uid: -1, Gid: -1}, nil}, // these should have been manifested by the assembler
-								{fs.Metadata{Name: "./d/d", Uid: -1, Gid: -1}, nil},
-								{fs.Metadata{Name: "./d/d/d"}, nil},
-								{fs.Metadata{Name: "./d/d/d/1"}, []byte{}},
-								{fs.Metadata{Name: "./d/d/d/2"}, []byte{}},
-								{fs.Metadata{Name: "./d/d/d/3"}, []byte{}},
-							}}.Defaults().Describe(filefixture.CompareDefaults),
-						)
-
-						if assembly != nil {
-							// conditional only because we may have continued moving after an error earlier.
-							assembly.Teardown()
-						}
-					})
-				})
+				// TODO
 			}),
 		),
 	)
+
+	Convey("Assembly with one placement into an implicitly-created dir works",
+		testutil.Requires(
+			testutil.RequiresRoot,
+			testutil.WithTmpdir(func() {
+				filefixture.Alpha.Create("./material/alpha")
+				filefixture.Beta.Create("./material/beta")
+				// TODO
+			}),
+		),
+	)
+
+	Convey("Assembly using the same base twice works",
+		testutil.Requires(
+			testutil.RequiresRoot,
+			testutil.WithTmpdir(func() {
+				filefixture.Alpha.Create("./material/alpha")
+				filefixture.Beta.Create("./material/beta")
+				// TODO
+			}),
+		),
+	)
+
+	Convey("Assembly with implicitly created deep dirs works",
+		testutil.Requires(
+			testutil.RequiresRoot,
+			testutil.WithTmpdir(func() {
+				filefixture.Alpha.Create("./material/alpha")
+				filefixture.Beta.Create("./material/beta")
+				assembleAndScan(
+					assemblerFn,
+					[]integrity.AssemblyPart{
+						{TargetPath: "/", SourcePath: "./material/alpha"},
+						{TargetPath: "/a", SourcePath: "./material/beta"},
+						{TargetPath: "/d/d/d", SourcePath: "./material/beta"},
+					},
+					filefixture.Fixture{Files: []filefixture.FixtureFile{
+						{fs.Metadata{Name: ".", Mode: 0755, ModTime: time.Unix(1000, 2000)}, nil}, // even though the basedir was made by the assembler, this should have the rootfs's properties overlayed onto it
+						{fs.Metadata{Name: "./a"}, nil},                                           // this one's mode and times should be overlayed by the second mount
+						{fs.Metadata{Name: "./a/1"}, []byte{}},
+						{fs.Metadata{Name: "./a/2"}, []byte{}},
+						{fs.Metadata{Name: "./a/3"}, []byte{}},
+						{fs.Metadata{Name: "./b", Mode: 0750, ModTime: time.Unix(5000, 2000)}, nil},
+						{fs.Metadata{Name: "./b/c", Mode: 0664, ModTime: time.Unix(7000, 2000)}, []byte("zyx")},
+						{fs.Metadata{Name: "./d", Uid: -1, Gid: -1}, nil}, // these should have been manifested by the assembler
+						{fs.Metadata{Name: "./d/d", Uid: -1, Gid: -1}, nil},
+						{fs.Metadata{Name: "./d/d/d"}, nil},
+						{fs.Metadata{Name: "./d/d/d/1"}, []byte{}},
+						{fs.Metadata{Name: "./d/d/d/2"}, []byte{}},
+						{fs.Metadata{Name: "./d/d/d/3"}, []byte{}},
+					}}.Defaults(),
+				)
+			}),
+		),
+	)
+
+	// additional coverage todos:
+	// - failure path: placement that overlaps a file somewhere
+	// - everything about changes and ensuring they're isolated... deserves a whole battery
+}
+
+func assembleAndScan(assemblerFn integrity.Assembler, parts []integrity.AssemblyPart, expected filefixture.Fixture) {
+	Convey("Assembly should not blow up", FailureContinues, func() {
+		var assembly integrity.Assembly
+		So(func() {
+			assembly = assemblerFn("./assembled", parts)
+		}, ShouldNotPanic)
+
+		Convey("Filesystem should scan as the expected union", func() {
+			scan := filefixture.Scan("./assembled")
+			So(scan.Describe(filefixture.CompareDefaults), ShouldEqual, expected.Describe(filefixture.CompareDefaults))
+
+			if assembly != nil {
+				// conditional only because we may have continued moving after an error earlier.
+				assembly.Teardown()
+			}
+		})
+	})
+}
+
+func CheckAssemblerSequestersData(assemblerFn integrity.Assembler) {
+	// TODO outputs will rely on these behaviors
+}
+
+func CheckAssemblerRespectsReadonly(assemblerFn integrity.Assembler) {
+	// TODO
 }
