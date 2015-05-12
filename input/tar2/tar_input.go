7d556baae88c6042ef4b721335083b0e0d166978
Eric Myhre <hash@exultant.us>
2015-05-11 20:19:19 -0700

    Attach hashes structurally to HashMismatchErrors.
    Sticking values irretreivably in fmt strings is a poor idea.
    
    Also, standardize the way HashMismatchError are created, so their fmt strings aren't copypasta'd around.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- input/tar2/tar_input.go
+++ input/tar2/tar_input.go
@@ -80,7 +80,7 @@ func (i Input) Apply(destinationRoot string) <-chan error {
 			// verify total integrity
 			expectedTreeHash, err := base64.URLEncoding.DecodeString(i.spec.Hash)
 			if !bytes.Equal(actualTreeHash, expectedTreeHash) {
-				done <- input.InputHashMismatchError.New("expected hash %q, got %q", i.spec.Hash, base64.URLEncoding.EncodeToString(actualTreeHash))
+				done <- input.NewHashMismatchError(i.spec.Hash, base64.URLEncoding.EncodeToString(actualTreeHash))
 			}
 		}).Catch(input.Error, func(err *errors.Error) {
 			done <- err
