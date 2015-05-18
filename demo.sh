73e1f6909f2b0866021c7170a02a58ab17aad762
Eric Myhre <hash@exultant.us>
2015-05-17 22:33:36 -0700

    Say what we're running as we go.  Show helps.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- demo.sh
+++ demo.sh
@@ -6,12 +6,17 @@ if [ "$1" != "-t" ]; then straight=true; fi; export straight;
 demodir="demo";
 
 cnbrown="$(echo -ne "\E[0;33m")" # prompt
+clblack="$(echo -ne "\E[1;30m")" # aside
+clbrown="$(echo -ne "\E[1;33m")" # command
 clblue="$(echo -ne "\E[1;34m")"  # section docs
 cnone="$(echo -ne "\E[0m")"
 awaitack() {
 	[ "$straight" != true ] && return;
 	echo -ne "${cnbrown}waiting for ye to hit enter, afore the voyage heave up anchor and make headway${cnone}"
 	read -es && echo -ne "\E[F\E[2K\r"
+}
+tellRunning() {
+	echo -e "${clblack}# running \`${clbrown}$@${clblack}\` >>>${cnone}"
 }
 
 rm -rf "$demodir"
@@ -24,6 +29,7 @@ echo "$demodir"
 echo -e "${clblue}# Repeatr says hello!${cnone}"
 echo -e "${clblue}#  Without a command, it provides help.${cnone}"
 (
+	tellRunning "repeatr"
 	repeatr
 )
 echo -e "${clblue} ----------------------------${cnone}\n\n"
@@ -33,6 +39,9 @@ awaitack
 echo -e "${clblue}# To suck in data, use the scan command:${cnone}"
 echo
 (
+	tellRunning "repeatr scan --help"
+	repeatr scan --help
+	tellRunning "repeatr scan --kind=tar"
 	repeatr scan --kind=tar
 )
 echo
@@ -45,10 +54,10 @@ awaitack
 
 
 
-echo -e "${clblue}# \`repeatr run\` takes a job description and executes it.${cnone}"
-echo -e "${clblue}#  Stdout goes to your terminal;${cnone}"
-echo -e "${clblue}#   Any 'output' specifications are saved/uploaded.${cnone}"
+echo -e "${clblue}# The \`repeatr run\` command takes a job description and executes it.${cnone}"
+echo -e "${clblue}#  Stdout goes to your terminal; any 'output' specifications are saved/uploaded.${cnone}"
 (
+	tellRunning "repeatr run -i some-json-config-files.conf"
 	repeatr run -i <(cat <<-EOF
 	{
 		"Inputs": [
@@ -73,4 +82,10 @@ echo -e "${clblue}#   Any 'output' specifications are saved/uploaded.${cnone}"
 EOF
 	)
 )
+echo -e "${clblue} ----------------------------${cnone}\n\n"
+awaitack
+
+
+echo "${clblue}#  That's all!  Neat, eh?${cnone}"
+
 
