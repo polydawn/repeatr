162c14285f111071c3a04ff1559490c943308e89
Eric Myhre <hash@exultant.us>
2015-05-22 11:15:28 -0700

    Document where filters should be applied.
    Our config currently doesn't actually allow filters on inputs, but it should evolve to do so, so that filters for e.g. UID can be used to make data materialization a non-priviledged operation.  But yes, that will cross-cut a fairly alarming amount of code.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/filter/filter.go
+++ io/filter/filter.go
@@ -7,6 +7,13 @@
 	the "modified time" properties from files -- since executing most
 	applications results in a spray of unique and unrepeatable mtime properties,
 	it's usually best to discard them.
+
+	Typically, filters should be applied on output scans -- it makes sense
+	to do data normalization *before* warehousing, both on general principle,
+	and because it will make data deduplicate better.  However, filters
+	may also be used on input/materialization; this may be useful in
+	circumstances where the data committed to warehouse has various UIDs,
+	but the local environment wants to get them all as the current UID.
 */
 package filter
 
