3cdfb4ffd237481452fa7247678a79061550017b
Eric Myhre <hash@exultant.us>
2015-06-01 16:27:33 -0500

    Migrate tarexec output to transmats.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/transmat/tarexec/tarexec_transmat.go
+++ io/transmat/tarexec/tarexec_transmat.go
@@ -3,19 +3,24 @@ package tarexec
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"syscall"
 
+	"github.com/polydawn/gosh"
 	"github.com/spacemonkeygo/errors"
+	"github.com/spacemonkeygo/errors/try"
 	"polydawn.net/repeatr/def"
 	"polydawn.net/repeatr/input"
 	tar_in "polydawn.net/repeatr/input/tar"
 	"polydawn.net/repeatr/io"
-	tar_out "polydawn.net/repeatr/output/tar"
 )
 
-var _ integrity.Transmat = &DirTransmat{}
+const Kind = integrity.TransmatKind("tar")
 
-type DirTransmat struct {
+var _ integrity.Transmat = &TarExecTransmat{}
+
+type TarExecTransmat struct {
 	workPath string
 }
 
@@ -24,15 +29,15 @@ var _ integrity.TransmatFactory = New
 func New(workPath string) integrity.Transmat {
 	err := os.MkdirAll(workPath, 0755)
 	if err != nil {
-		panic(input.TargetFilesystemUnavailableIOError(err)) // TODO these errors should migrate
+		panic(integrity.TransmatError.New("Unable to set up workspace: %s", err))
 	}
-	return &DirTransmat{workPath}
+	return &TarExecTransmat{workPath}
 }
 
 /*
 	Arenas produced by Dir Transmats may be relocated by simple `mv`.
 */
-func (t *DirTransmat) Materialize(
+func (t *TarExecTransmat) Materialize(
 	kind integrity.TransmatKind,
 	dataHash integrity.CommitID,
 	siloURIs []integrity.SiloURI,
@@ -68,24 +73,56 @@ func (t *DirTransmat) Materialize(
 	return arena
 }
 
-func (t DirTransmat) Scan(
+func (t TarExecTransmat) Scan(
 	kind integrity.TransmatKind,
 	subjectPath string,
 	siloURIs []integrity.SiloURI,
 	options ...integrity.MaterializerConfigurer,
 ) integrity.CommitID {
-	if len(siloURIs) <= 0 {
-		// odd hack, replace with actual comprehensive of uri lists when finishing migrating.
-		siloURIs = []integrity.SiloURI{"/dev/null"}
-	}
-	report := <-tar_out.New(def.Output{
-		Type: string(kind),
-		URI:  string(siloURIs[0]),
-	}).Apply(subjectPath)
-	if report.Err != nil {
-		panic(report.Err)
-	}
-	return integrity.CommitID(report.Output.Hash)
+	try.Do(func() {
+		// Basic validation and config
+		if kind != Kind {
+			panic(errors.ProgrammerError.New("This transmat supports definitions of type %q, not %q", Kind, kind))
+		}
+
+		// Parse save locations.
+		//  (Most transmats do... significantly smarter things than this backwater.)
+		var localPath string
+		if len(siloURIs) == 0 {
+			localPath = "/dev/null"
+		} else if len(siloURIs) == 1 {
+			// TODO still assuming all local paths and not doing real uri parsing
+			localPath = string(siloURIs[0])
+			err := os.MkdirAll(filepath.Dir(localPath), 0755)
+			if err != nil {
+				panic(integrity.WarehouseConnectionError.New("Unable to write file: %s", err))
+			}
+			file, err := os.OpenFile(localPath, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				panic(integrity.WarehouseConnectionError.New("Unable to write file: %s", err))
+			}
+			file.Close() // just checking, so we can (try to) give a more pleasant error than tar barf
+		} else {
+			panic(integrity.ConfigError.New("%s transmat only supports shipping to 1 warehouse", Kind))
+		}
+
+		// exec tar.
+		// in case of a zero (a.k.a. success) exit, this returns silently.
+		// in case of a non-zero exit, this panics; the panic will include the output.
+		gosh.Gosh(
+			"tar",
+			"-cf", localPath,
+			"--xform", "s,"+strings.TrimLeft(subjectPath, "/")+",.,",
+			subjectPath,
+			gosh.NullIO,
+		).RunAndReport()
+	}).Catch(integrity.Error, func(err *errors.Error) {
+		panic(err)
+	}).CatchAll(func(err error) {
+		panic(integrity.UnknownError.Wrap(err))
+	}).Done()
+	return ""
+
 }
 
 type dirArena struct {
