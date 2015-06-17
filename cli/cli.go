5c2e2b7417dd418ddd3e5319a10eed120d9bd7fe
Eric Myhre <hash@exultant.us>
2015-06-16 19:35:20 -0500

    Nonexistent subcommands are an error.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cli/cli.go
+++ cli/cli.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/codegangsta/cli"
 )
@@ -18,6 +20,13 @@ func Main(args []string, journal, output io.Writer) {
 	App.Commands = []cli.Command{
 		RunCommandPattern(journal),
 		ScanCommandPattern(journal, output),
+	}
+
+	// Reporting "no help topic for 'zyx'" and exiting with a *zero* is... silly.
+	// A failure to hit a command should be an error.  Like, if a bash script does `repeatr somethingimportant`, there's no way it shouldn't *stop* when that's not there.
+	App.CommandNotFound = func(ctx *cli.Context, command string) {
+		fmt.Fprintf(ctx.App.Writer, "'%s %v' is not a repeatr subcommand\n", ctx.App.Name, command)
+		os.Exit(int(EXIT_BADARGS))
 	}
 
 	App.Run(args)
