c7b1b49be4aa1cec4ef5625ab69c5742f0a143c6
Eric Myhre <hash@exultant.us>
2015-06-24 17:55:11 -0500

    Thought about user experience with filters.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/filter/uid_filter.go
+++ io/filter/uid_filter.go
@@ -4,6 +4,11 @@ import (
 	"polydawn.net/repeatr/lib/fs"
 )
 
+// TODO While the user is allowed to do whatever they want, somewhere up near
+//  the config layer we should probably detect if any inputs use uid/gid
+//  filtering to avoid chowns, and if that formula doesn't also spec uid/gid
+//  filtering to normalize the outputs, we should emit a helpful warning message.
+
 var _ Filter = UidFilter{}
 
 type UidFilter struct {
