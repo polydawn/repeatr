634aab38bd9c6f689b4d295513f18f9e007be1ab
Eric Myhre <hash@exultant.us>
2015-11-09 17:24:19 -0600

    Fence tests warehouses in tempdirs.
    Previous failure to do so is rather gross.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- io/transmat/dir/dir_transmat_test.go
+++ io/transmat/dir/dir_transmat_test.go
@@ -26,7 +26,9 @@ func TestCoreCompliance(t *testing.T) {
 		cwd, _ := os.Getwd()
 		tests.CheckRoundTrip(Kind, New, "file://"+filepath.Join(cwd, "bounce"), "file literal", "absolute")
 		// round-trip using content-addressible "warehouse"
-		os.Mkdir("bounce", 0755) // make the warehouse location
-		tests.CheckRoundTrip(Kind, New, "file+ca://bounce", "content-addressible")
+		testutil.WithTmpdir(func() {
+			os.Mkdir("bounce", 0755) // make the warehouse location
+			tests.CheckRoundTrip(Kind, New, "file+ca://bounce", "content-addressable")
+		})(nil)
 	}))
 }
