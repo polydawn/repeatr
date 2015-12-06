7bf2596af22a1a2a7c0f1ba5c6a2d5497609b3ad
Eric Myhre <hash@exultant.us>
2015-12-05 22:13:20 -0600

    Implement codec for OutputGroup.
    This is all but entirely copypasta of InputGroup (and I refactored names of some of the map->slice code in inputgroup to be literally copypasta) -- on some levels this is okay; on others, frustating.  The two *are* deeply similar, but making them share more code by field embedding has been considered before and rejected as currently not sufficient ROI; more frustratingly, the map->slice code... currently, I'm aware of no way to do it within the confines of golang without copy paste (this is a scenario where one might choose to invoke go-generate to template things, if it got much further out of hand).
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- def/ware_serial.go
+++ def/ware_serial.go
@@ -19,20 +19,20 @@ func (ig *InputGroup) CodecDecodeSelf(c *codec.Decoder) {
 	c.MustDecode((*map[string]*Input)(ig))
 }
 
-func (ig InputGroup) asMappySlice() codec.MapBySlice {
-	keys := make([]string, len(ig))
+func (mp InputGroup) asMappySlice() codec.MapBySlice {
+	keys := make([]string, len(mp))
 	var i int
-	for k, _ := range ig {
+	for k, _ := range mp {
 		keys[i] = k
 		i++
 	}
 	sort.Strings(keys)
-	val := make(mappySlice, len(ig)*2)
+	val := make(mappySlice, len(mp)*2)
 	i = 0
 	for _, k := range keys {
 		val[i] = k
 		i++
-		val[i] = ig[k]
+		val[i] = mp[k]
 		i++
 	}
 	return val
@@ -41,5 +41,42 @@ func (ig InputGroup) asMappySlice() codec.MapBySlice {
 func (i Input) String() string {
 	var buf bytes.Buffer
 	codec.NewEncoder(&buf, &codec.JsonHandle{}).Encode(i)
+	return buf.String()
+}
+
+var _ codec.Selfer = &OutputGroup{}
+
+func (og OutputGroup) CodecEncodeSelf(c *codec.Encoder) {
+	c.Encode(og.asMappySlice())
+}
+
+func (og *OutputGroup) CodecDecodeSelf(c *codec.Decoder) {
+	// I'd love to just punt to the defaults, but the `Selfer` interface doesn't come in half.
+	// SO here's a ridiculous indirection to prance around infinite recursion.
+	c.MustDecode((*map[string]*Output)(og))
+}
+
+func (mp OutputGroup) asMappySlice() codec.MapBySlice {
+	keys := make([]string, len(mp))
+	var i int
+	for k, _ := range mp {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	val := make(mappySlice, len(mp)*2)
+	i = 0
+	for _, k := range keys {
+		val[i] = k
+		i++
+		val[i] = mp[k]
+		i++
+	}
+	return val
+}
+
+func (o Output) String() string {
+	var buf bytes.Buffer
+	codec.NewEncoder(&buf, &codec.JsonHandle{}).Encode(o)
 	return buf.String()
 }
