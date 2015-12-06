9505373f3b0aaac0d0316767d08ec4ca02f8e9cd
Eric Myhre <hash@exultant.us>
2015-12-06 01:30:57 -0600

    Fix encoding recursion goof on WarehouseCoords.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- def/where.go
+++ def/where.go
@@ -20,7 +20,7 @@ func (wc WarehouseCoords) CodecEncodeSelf(c *codec.Encoder) {
 		// note: no, we're not sorting:
 		//  1) nah, don't care.  not part of conjecture anyway.
 		//  2) order actually has a meaning: order in which to try.
-		c.MustEncode(wc)
+		c.MustEncode([]string(wc))
 	}
 }
 
