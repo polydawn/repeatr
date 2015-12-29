28fe2c09a37cf6677b5b7b515fa8af0a6c0a4840
Eric Myhre <hash@exultant.us>
2015-12-28 21:35:03 -0600

    More than one kind of work planner can be expected in the future.
    For now, of course, this is only worth discussing in that it informs scope limits for the first pass.  So here's comments on that.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- catalog/watcher.go
+++ catalog/watcher.go
@@ -16,3 +16,17 @@ type Watchable interface {
 	*/
 	Subscribe(chan<- Catalog)
 }
+
+// LATER:
+//   We may also end up with a system of notifiers for newly recorded stage3
+// execution completions, as well as task leases.  The first gen work planner
+// is going to make simplifying assumptions about a single executor (it's local)
+// and the main source of complicated interactions with the work heap is going
+// to be cancellation if a new catalog edition comes out that makes current
+// planned work less interesting -- but other events may be part of the mix in
+// the future: some work items will be queued multiple times (i.e. check reps),
+// some tasks may need to be farmed to different label groups (controlling for
+// hardware), sometimes the goal is broad coverage and not just latest stuff,
+// etc.  So, in short, planning details should not leak directly into
+// anything about catalogs or run record and formula database features;
+// those things need to be attached to and scoped within work planners.
