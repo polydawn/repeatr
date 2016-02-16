c9ca3cebb2a8903a908d7ef1fc618948516755f2
Eric Myhre <hash@exultant.us>
2016-02-15 18:51:15 -0800

    cradle.ensureTempDir and tests.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- executor/cradle/cradle.go
+++ executor/cradle/cradle.go
@@ -13,7 +13,10 @@ import (
 	`ApplyDefaults` first (this refers to cwd).
 */
 func MakeCradle(rootfsPath string, frm def.Formula) {
-
+	ensureWorkingDir(rootfsPath, frm)
+	ensureHomeDir(rootfsPath, frm.Action.Policy)
+	ensureTempDir(rootfsPath)
+	ensureIdentity(rootfsPath, frm.Action.Policy)
 }
 
 // TODO : also support support empty dir as an input type for freehand
@@ -26,8 +29,32 @@ func ensureHomeDir(rootfsPath string, policy def.Policy) {
 
 }
 
-func ensureTempDir(rootfsPath string, policy def.Policy) {
+/*
+	Ensure `/tmp` exists and anyone can write there.
+	The sticky bit will be applied and permissions set to 777.
 
+	Edge case note: will follow symlinks.
+*/
+func ensureTempDir(rootfsPath string) {
+	pth := filepath.Join(rootfsPath, "/tmp")
+	stickyMode := os.FileMode(0777) | os.ModeSticky
+	// try to chmod first
+	err := os.Chmod(pth, stickyMode)
+	if err == nil {
+		return
+	}
+	// for unexpected complaints, bail
+	if !os.IsNotExist(err) {
+		panic(err) // TODO category for here??
+	}
+	// mkdir if not exist
+	if err := os.Mkdir(pth, stickyMode); err != nil {
+		panic(err) // TODO category for here??
+	}
+	// chmod it *again* because unit tests reveal that `os.Mkdir` is subject to umask
+	if err := os.Chmod(pth, stickyMode); err != nil {
+		panic(err) // TODO category for here??
+	}
 }
 
 /*
