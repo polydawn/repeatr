f78c400e94c5eda0c6beb665ce3ad47ed17840b4
Eric Myhre <hash@exultant.us>
2016-02-15 17:26:55 -0800

    More docs on Policy; better names of consts.
    The new names for the Policy constants are specifically chosen to avoid mentioning "root", because that's been *immediately* a seed of confusion in every conversation about the subject over the last few days (including in my own earlier docs about fakeroot: it's clearly inaccurate to say that needing uid=0 to alter files owned by uid=0 is an incorrect request for "root", natch, right?).
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- executor/cradle/cradle.go
+++ executor/cradle/cradle.go
@@ -30,6 +30,24 @@ func ensureTempDir(rootfsPath string, policy Policy) {
 
 }
 
+/*
+	Ensures the MVP filesystem considers configuration for the appropriate
+	user account.
+
+	Which is "the appropriate user account" varies according to the Policy.
+
+	We define identity in terms that `nsswitch` would refer to as "compat":
+	the ancient `/etc/{passwd,group,shadow}` files, because these are the
+	most widely understood formats, and tend to keep working even in
+	non-dynamically-linked programs (and different libc implementations,
+	etc etc etc).
+
+	We do not screw with a `/etc/nsswitch.conf` file if one exists, nor
+	alter our behavior based on it -- cradle is about enforcing *absolute*
+	minimum viable behaviors and fallbacks, not parsing and smoothing
+	every concievable fractal of at-one-time-in-history-valid configuration.
+
+*/
 func ensureIdentity(rootfsPath string, policy Policy) {
 
 }
