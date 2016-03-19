cf757471128b9c43caf508f20fd24a03d9bdf484
Eric Myhre <hash@exultant.us>
2016-03-19 15:16:33 -0700

    Remove last reference to oldold assets system.
    ... by having a crappy network dependency.  Not actually good; I'm just antsy about getting rid of that dir.  We currently also have a network dep with tests for runc... so, we already need to solve the assets-in-test problem better and the same solution will apply to *both* of those to get back to zero-network build&test cycles.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- core/executor/tests/util.go
+++ core/executor/tests/util.go
@@ -1,27 +1,23 @@
 package tests
 
 import (
-	"path/filepath"
-
 	"polydawn.net/repeatr/def"
-	"polydawn.net/repeatr/lib/testutil"
 )
 
 /*
 	Return an otherwise-blank formula that has a sane rootfs.
 */
 func getBaseFormula() def.Formula {
-	// find local assets.  we rely on local files bootstrapped by earlier build process steps rather than have executor tests depend on networked transmats (and thus *network*).
-	// is janky.  don't know of a best practice for finding your "project dir".  we just assume everyone calling this is a test two dirs deep :I
-	projPath := filepath.Dir(filepath.Dir(filepath.Dir(testutil.OriginalDir())))
-
+	// TODO this should have a local mirror
 	return def.Formula{
 		Inputs: def.InputGroup{
 			"main": {
-				Type:       "tar",
-				MountPath:  "/",
-				Hash:       "uJRF46th6rYHt0zt_n3fcDuBfGFVPS6lzRZla5hv6iDoh5DVVzxUTMMzENfPoboL",
-				Warehouses: []string{"file://" + filepath.Join(projPath, "assets/ubuntu.tar.gz")},
+				Type:      "tar",
+				MountPath: "/",
+				Hash:      "aLMH4qK1EdlPDavdhErOs0BPxqO0i6lUaeRE4DuUmnNMxhHtF56gkoeSulvwWNqT",
+				Warehouses: []string{
+					"http+ca://repeatr.s3.amazonaws.com/assets/",
+				},
 			},
 		},
 	}
