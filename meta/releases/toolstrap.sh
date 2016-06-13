dde58a29e773413c67b9afc08d2b8fe31e9e3bac
Eric Myhre <hash@exultant.us>
2016-06-13 12:42:38 -0700

    More docs to toolstrap.
    I was thinking about using this to do the asset-cache bootstrap, but it's basically a case study in why you shouldn't try to carry this stuff too far in bash: the asset-cache expects an ever-so-slightly different dir layout, and not untar'ing.  If one implements enough flags to cover all this, it ends up more complex than hand-rolling the downloads all over again in another script...
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- meta/releases/toolstrap.sh
+++ meta/releases/toolstrap.sh
@@ -7,15 +7,41 @@
 ## over time, but they don't fight for space or overwrite each other (useful when
 ## jumping across git branches that have different requirements, for example).
 ##
-## This is just an example script (with invocation of a fetch at the bottom).
-## To use it in your project, copy it into your own git repo, and replace
-## those last lines with your own values (or, source it into another script
-## and call the toolstrap function from there).
+## Why use this instead of wget/curl/hash yourself?  Toolstrap avoids the
+## following common pitfalls:
+##  - Toolstrap's dir structure avoids collisions
+##  - Toolstrap produces nice output on hash mismatch
+##  - Toolstrap manages atomicity -- no partial download problems
+##  - Toolstrap manages atomicity -- no partial extraction problems
+##
+## To use toolstrap in your project, copy it into your own git repo,
+## source it into your own scripts,
+## and call the toolstrap function from there.
+##
+## The dir structure comes out like this:
+##  tools/${tool_name}/${version_name}/*
+## Note that this does not include the hash you used to download it; the cache
+## is *not* content addressible, and you can easily manually break it.
 ##
 ## Hopefully you won't need this for more than once, of course ;)
 ## Once you have Repeatr, it can do a better job of managing CAS, dedup,
 ## multiple mirrors, network transparency, etc etc for you :)
 ##
+
+## USAGE EXAMPLE:
+##
+## ```
+##	mkdir -p tools/
+##	toolstrap \
+##		repeatr \
+##		v0.12 \
+##		61ef917c7988d985629a4818858dbc614cb7a6da6c37c2a6bcf6cf97781fc5c83f028243d4c11a2b7d958a1c78fa6c6b \
+##	 	https://github.com/polydawn/repeatr/releases/download/release%2Fv0.12/repeatr-linux-amd64-v0.12.tar.gz
+## ```
+##
+## The downloaded file will be extracted into
+##  ./tools/repeatr/v0.12/*
+
 
 toolstrap() {(
 	set -euo pipefail
@@ -39,6 +65,8 @@ toolstrap() {(
 	rm -rf "${tmpdl}" "${tmpdir}" || true;
 
 	### Download and check hash against expectation.
+	# (This eschews use of '--check' options so it can produce better output,
+	# specifically including the actual hash in the case of the check failing.)
 	wget -O "${tmpdl}" "${URL}"
 	sha384sum "${tmpdl}" | tee /dev/fd/2 | grep ^"${HASH} " >/dev/null \
 		|| { echo "corrupt or hash mismatched ${NAME}-${VERSIONLABEL} download" 1>&2; return 16; }
@@ -51,10 +79,3 @@ toolstrap() {(
 	mv "${tmpdir}" "${destdir}"
 	rm -f "${tmpdl}"
 )}
-
-mkdir -p tools/
-toolstrap \
-	repeatr \
-	v0.12 \
-	61ef917c7988d985629a4818858dbc614cb7a6da6c37c2a6bcf6cf97781fc5c83f028243d4c11a2b7d958a1c78fa6c6b \
-	https://github.com/polydawn/repeatr/releases/download/release%2Fv0.12/repeatr-linux-amd64-v0.12.tar.gz
