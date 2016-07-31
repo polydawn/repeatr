875e349ac5c7e7fd2f3eb6979a70322d6d95bd1c
Eric Myhre <hash@exultant.us>
2016-07-30 20:40:14 -0500

    Fix executor dispatch to return a local err type.
    Removes one more spacemonkey-typed error from the def package.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- api/def/errors.go
+++ api/def/errors.go
@@ -4,14 +4,6 @@ import (
 	"github.com/spacemonkeygo/errors"
 )
 
-/*
-	Validation error is a base class for anything that matches the description
-	of an HTTP 400.  (Unless the validation should have been performed at an
-	earlier stage, and the current check is only for sanity; then, if it fails
-	and it's considered a compile-time boo boo, use `errors.ProgrammerError`.)
-*/
-var ValidationError *errors.ErrorClass = errors.NewClass("ValidationError")
-
 var ConfigError *errors.ErrorClass = errors.NewClass("ConfigError")
 
 func newConfigValTypeError(expectedKey, mustBeA string, wasActually string) *errors.Error {
