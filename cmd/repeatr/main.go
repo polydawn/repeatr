3dc2e1f98ced5debe1e86081712621bcb3738c6a
Eric Myhre <hash@exultant.us>
2016-10-06 17:59:42 -0700

    Start re-orienting the repeatr cli cmd setup code to what's emerged as best practice from other projects.
    Dropping all of the custom error handling we did on the ground outright.  It was undertested and a mess.  It will be re-created based on meep errors... and even then, we'll probably give a pass to much of this file saving nonsense and especially eschew the custom stack stuff here, because meep actually handles those things coherently and printably.
    
    Next step: demolish the repeatr//core/cli package, one command at a time, moving each of them to their own packages.  They shouldn't cross talk to each other at all, and contain a pretty minimal amount of args parsing.  Anything bigger than that (say, the code shared by the twerk and run subcommands right now) shall deserve its own package and reasonable API maintained at that distance.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ cmd/repeatr/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/codegangsta/cli"
+
+	rcli "go.polydawn.net/repeatr/core/cli"
+)
+
+func main() {
+	os.Exit(Main(os.Args, os.Stdin, os.Stdout, os.Stderr))
+}
+
+const (
+	EXIT_SUCCESS      = 0
+	EXIT_BADARGS      = 1
+	EXIT_UNKNOWNPANIC = 2  // same code as golang uses when the process dies naturally on an unhandled panic.
+	EXIT_JOB          = 10 // used to indicate a job reported a nonzero exit code (from cli commands that execute a single job).
+	EXIT_USER         = 3  // grab bag for general user input errors (try to make a more specific code if possible/useful)
+)
+
+func Main(
+	args []string,
+	stdin io.Reader,
+	stdout io.Writer,
+	stderr io.Writer,
+) (exitcode int) {
+	app := &cli.App{
+		Name:      "repeatr",
+		Usage:     "Run it. Run it again.",
+		UsageText: "Repeatr runs processes in containers, provisioning their inputs and saving their outputs using reliable, immutable, content-addressable goodness.",
+		Version:   "v0.13+dev",
+		Writer:    stderr,
+		Commands: []cli.Command{
+			rcli.RunCommandPattern(stdout, stderr),
+			rcli.TwerkCommandPattern(stdin, stdout, stderr),
+			rcli.UnpackCommandPattern(stderr),
+			rcli.ScanCommandPattern(stdout, stderr),
+			rcli.ExploreCommandPattern(stdout, stderr),
+			rcli.CfgCommandPattern(stdin, stdout, stderr),
+			{
+				Name:  "version",
+				Usage: "Shows the version of repeatr",
+				Action: func(ctx *cli.Context) {
+					cli.ShowVersion(ctx)
+				},
+			},
+		},
+		CommandNotFound: func(ctx *cli.Context, command string) {
+			exitcode = EXIT_BADARGS
+			fmt.Fprintf(stderr, "Incorrect usage: '%s' is not a %s subcommand\n", command, ctx.App.Name)
+		},
+		Action: func(ctx *cli.Context) error {
+			if ctx.Args().Present() {
+				cli.ShowCommandHelp(ctx, ctx.Args().First())
+			} else {
+				cli.ShowAppHelp(ctx)
+			}
+			return nil
+		},
+	}
+	cli.VersionPrinter = func(ctx *cli.Context) {
+		// Put some more info in our version printer.
+		// Also, version goes to stdout.
+		fmt.Fprintf(os.Stdout, "%v %v\n", ctx.App.Name, ctx.App.Version)
+		fmt.Fprintf(os.Stdout, "git commit %v\n", rcli.GITCOMMIT)
+		fmt.Fprintf(os.Stdout, "build date %v\n", rcli.BUILDDATE)
+	}
+	if err := app.Run(args); err != nil {
+		exitcode = EXIT_BADARGS
+		fmt.Fprintf(stderr, "Incorrect usage: %s", err)
+	}
+	return
+}
