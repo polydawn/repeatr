f15d2de2e0cbf59dcef822e5fac3a5451fc0dfc2
Eric Myhre <hash@exultant.us>
2016-10-07 16:55:12 -0700

    Draft of a single coherent controller object that can be a sink for all the output an executor generates and implement the act.RunObserver API in one place.
    This is from the wee hours of the morning and might not hold up to scrutiny.  In particular I was think of having some sort of arcane rendevouz system in place to avoid the need for buffering until the Follow is called.  But something simpler should be possible.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ core/actors/runner/flipper.go
@@ -0,0 +1,70 @@
+package runner
+
+import (
+	"io"
+
+	"github.com/inconshreveable/log15"
+
+	"go.polydawn.net/repeatr/api/act"
+	"go.polydawn.net/repeatr/api/def"
+)
+
+var _ act.RunObserver = &flipper{}
+
+/*
+	Implements act.RunObserver and manufactures the sinks (the logger,
+	journal, etc) to hand to the executor.
+*/
+type flipper struct {
+	runID  def.RunID
+	stream chan<- *def.Event
+}
+
+func (x *flipper) FollowEvents(
+	which def.RunID,
+	stream chan<- *def.Event,
+	_ def.EventSeqID,
+) {
+
+}
+
+func (x *flipper) GetLogger() log15.Logger {
+	log := log15.New()
+	log.SetHandler(log15.FuncHandler(func(r *log15.Record) error {
+		x.stream <- &def.Event{
+			RunID: x.runID,
+			Log: &def.LogItem{
+				Level: int(r.Lvl),
+				Msg:   r.Msg,
+				Ctx:   r.Ctx,
+				Time:  r.Time,
+			},
+		}
+		return nil
+	}))
+	return log
+}
+
+type journalEventWriter struct {
+	runID  def.RunID
+	stream chan<- *def.Event
+}
+
+func (w *journalEventWriter) Write(b []byte) (int, error) {
+	w.stream <- &def.Event{
+		RunID:   w.runID,
+		Journal: string(b),
+	}
+	return len(b), nil
+}
+
+func (x *flipper) GetJournal() io.Writer {
+	return &journalEventWriter{x.runID, x.stream}
+}
+
+func (x *flipper) Result(rr *def.RunRecord) {
+	x.stream <- &def.Event{
+		RunID:     x.runID,
+		RunRecord: rr,
+	}
+}
