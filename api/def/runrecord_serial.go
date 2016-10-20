9f90982ce1f0843f70afc4ff95474ef131186927
Eric Myhre <hash@exultant.us>
2016-10-20 11:20:38 -0700

    This rabbithole is WILD.
    See comments.  I can't believe this is the only way to do this.  I *must* have missed something.  But I'm sure I don't know what.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- api/def/runrecord_serial.go
+++ api/def/runrecord_serial.go
@@ -80,6 +80,35 @@ type runRecordFailureEnvelope struct {
 	Detail interface{} `json:"detail"`
 }
 
+// don't care, use defaults... by having another anon struct.
+func (fe runRecordFailureEnvelope) CodecEncodeSelf(c *codec.Encoder) {
+	// Seriously, my kingdom for a less ridiculous dance here to say "DTRT".
+	// We couldn't even do the inline during the already-silly custom encode
+	//  func on runrecord, because we need a struct to implement `error` -.-
+	// Nor could we simply *leave this method off*, because you can't do
+	//  one direction only to implement the ugorji `Selfer` interface.
+	// Oh, one more travesty: we ended up having to build a *custom decode*
+	//  entirely, and in order to not make it spirallingly complex, I only
+	//  made it support parsing where the type comes first (because otherwise
+	//  I have to buffer the detail part again... in a way that I can't
+	//  feed back into the codec, nor again can we spawn a new codec to handle
+	//  a fresh byte stream while using the same handle and config.  Wow).
+	//  WHICH MEANS.  We can't even use the anon struct; we need to go
+	//  full on order override to make sure this can round-trip.
+	c.MustEncode(mappySlice{
+		"type", fe.Type,
+		"detail", fe.Detail,
+	})
+}
+
+// this method had to get WAY too fancy in order to do some basic polymorphism.
+func (fe *runRecordFailureEnvelope) CodecDecodeSelf(c *codec.Decoder) {
+	_, dec := codec.GenHelperDecoder(c)
+	dec.ReadMapStart()
+	// i need the type info before i can decode the value, but it wasn't exported in that order
+	// is there an exported ability to consume the end-of-map token?! i can't see one.
+}
+
 func (runRecordFailureEnvelope) Error() string { return "" }
 
 func failureTypeToString(e error) string {
