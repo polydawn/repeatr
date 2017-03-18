93370747dcf815a0be4293ad2172ff29df392891
Eric Myhre <hash@exultant.us>
2017-03-17 22:58:15 -0700

    Add tests for Formula.Hash to match some fixtures.
    Repeated runs of this test confirms that no inappropriate randomness (e.g. map key orderings) is creeping in to the serialization function used.  Coverage also confirms that some fields that should not be hashed aren't; and some that should are.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ api/def/formula_hash_test.go
@@ -0,0 +1,45 @@
+package def_test
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	rdef "go.polydawn.net/repeatr/api/def"
+)
+
+func TestFormulaHashFixtures(t *testing.T) {
+	Convey("Formulas should hash consistently", t, func() {
+		Convey("Given fixture 1", func() {
+			frm := &rdef.Formula{
+				Inputs: rdef.InputGroup{
+					"rootfs": &rdef.Input{
+						Type:       "tar",
+						Hash:       "aLMH4qK1EdlPDavdhErOs0BPxqO0i6lUaeRE4DuUmnNMxhHtF56gkoeSulvwWNqT",
+						Warehouses: rdef.WarehouseCoords{"http+ca://repeatr.s3.amazonaws.com/assets/"},
+						MountPath:  "/",
+					},
+				},
+				Action: rdef.Action{
+					Entrypoint: []string{"bash", "-c", "echo hello && sleep 5 && echo yes"},
+				},
+			}
+			Convey("It should match the fixture", func() {
+				So(frm.Hash(), ShouldEqual, "7dCwntFg7FtcUNs4FcRaUknXWWskG889kypU9y3BpWdVmT4aMA76zkZyjaNYL1x989")
+			})
+			Convey("Given a fixture that varies only in warehouses, it should match the same fixture", func() {
+				frm.Inputs["rootfs"].Warehouses = rdef.WarehouseCoords{"file+ca://./local/"}
+				So(frm.Hash(), ShouldEqual, "7dCwntFg7FtcUNs4FcRaUknXWWskG889kypU9y3BpWdVmT4aMA76zkZyjaNYL1x989")
+			})
+			Convey("Given changes in outputs, it should have a different fixture", func() {
+				frm.Outputs = rdef.OutputGroup{
+					"product": &rdef.Output{
+						Type:      "tar",
+						MountPath: "/output",
+					},
+				}
+				So(frm.Hash(), ShouldEqual, "8h8bRDwDAS39QtyQ7SNn9BYKZkXCxkxWMpCcHAMKdbvYe2qQ3r7TduVccHeCe8dk4y")
+			})
+		})
+	})
+}
