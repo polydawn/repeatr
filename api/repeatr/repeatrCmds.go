e24f820b4b6e0d20b5ddccec1c962a6acaa74bae
Eric Myhre <hash@exultant.us>
2017-07-22 18:35:53 -0500

    api: this rather odd idea we might have interfaces
    RPC is a dangerous water to stroll into but, well, we've already done it once, by necessity.  Let's have it in mind early as we do this fresh pass.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ api/repeatr/repeatrCmds.go
@@ -0,0 +1,45 @@
+/*
+	Interfaces of repeatr commands.
+
+	The real repeatr logic implements these;
+	so do the various proxy tools (e.g. r2k8s);
+	so do some mocks, which are useful for testing.
+
+	A serial client is included in this package;
+	how to launch any remote actions is not in this package.
+*/
+package repeatr
+
+import (
+	"context"
+	"time"
+
+	"go.polydawn.net/repeatr/api/rdef"
+)
+
+func Run(ctx context.Context, formula rdef.Formula, stream chan<- *Event) (rdef.RunRecord, error) {
+	return rdef.RunRecord{}, nil
+}
+
+/*
+	A "union" type of all the kinds of event that may be generated in the
+	course of running a formula.
+
+	(RunRecords aren't forwarded through 'stream' chans like this, but they're
+	defined in the union because it's about where they're sent on the wire.)
+*/
+type Event struct {
+	Log       *LogItem        `refmt:"log,omitempty"`       // Log events are repeatr's logs.
+	Journal   string          `refmt:"journal,omitempty"`   // User output strings are called journal events.
+	RunRecord *rdef.RunRecord `refmt:"runRecord,omitempty"` // RunRecords are the final event emitted by a run.
+}
+
+/*
+	Log events from repeatr as serialized.
+*/
+type LogItem struct {
+	Time  time.Time         `refmt:"t"`
+	Level int               `refmt:"lvl"`
+	Msg   string            `refmt:"msg"`
+	Ctx   map[string]string `refmt:"ctx,omitempty"`
+}
