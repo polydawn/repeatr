6fcae9780f029515694b960ec857dfad58211d1d
Eric Myhre <hash@exultant.us>
2017-09-29 10:48:04 +0200

    executor/impl/chroot: partial port of cmd build.
    Some of these pieces will be extractable, like the command path error checks.
    
    Some of these checks also might end up commented out until we get to a later cycle of development: doing them *correctly* for all sufficiently perverse filesystems is Tricky -- not impossible, but we need to advance the core functionality far enough that we can write tests for it, THEN come back and start writing test coverage to ensure we're getting the Tricky parts right.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- executor/impl/chroot/chrootExecutor.go
+++ executor/impl/chroot/chrootExecutor.go
@@ -2,6 +2,8 @@ package chroot
 
 import (
 	"context"
+	"os/exec"
+	"syscall"
 
 	. "github.com/polydawn/go-errcat"
 
@@ -55,12 +57,90 @@ func (cfg Executor) Run(
 	}()
 
 	// Invoke containment and run!
+	cmd := buildCmd(formula, chrootFs.BasePath())
 	// TODO
-	// DESIGN: this really only needs the `frm.Action` and the work dir...
+	_ = cmd
 
 	// Pack outputs.
 	// TODO
 
 	// Done!
 	return nil, nil
+}
+
+/*
+	Return an error if any part of the filesystem is invalid for running the
+	formula -- e.g. the CWD setting isn't a dir; the command binary
+	does not exist or is not executable; etc.
+
+	The formula is already expected to have been syntactically validated --
+	e.g. all paths have been checked to be absolute, etc.  This method will
+	panic if such invarients aren't held.
+
+	(It's better to check all these things before attempting to launch
+	containment because the error codes returned by kernel exec are sometimes
+	remarkably ambiguous or outright misleading in their names.)
+
+	Currently, we require exec paths to be absolute.
+*/
+func sanityCheckFs(frm api.Formula, chrootFs fs.FS) error {
+	// Check that the CWD exists and is a directory.
+	// FIXME this needs boxed symlink traversal to give correct answers
+	stat, err := chrootFs.LStat(fs.MustAbsolutePath(string(frm.Action.Cwd)).CoerceRelative())
+	if err != nil {
+		return Errorf(repeatr.ErrJobInvalid, "cwd invalid: %s", err)
+	}
+	if stat.Type != fs.Type_Dir {
+		return Errorf(repeatr.ErrJobInvalid, "cwd invalid: path is a %s, must be dir", stat.Type)
+	}
+
+	// Check that the command exists and is executable.
+	//  (If the format is not executable, that's another ball of wax, and
+	//  not so simple to detect, so we don't.)
+	stat, err = chrootFs.LStat(fs.MustAbsolutePath(frm.Action.Exec[0]).CoerceRelative())
+	if err != nil {
+		return Errorf(repeatr.ErrJobInvalid, "exec invalid: %s", err)
+	}
+	if stat.Type != fs.Type_File {
+		return Errorf(repeatr.ErrJobInvalid, "exec invalid: path is a %s, must be executable file", stat.Type)
+	}
+	// FUTURE: ideally we could also check if the file is properly executable,
+	//  and all parents have bits to be traversable (!), to the policy uid.
+	//  But this is also a loooot of work: and a correct answer (for groups
+	//  at least) requires *understanding the container's groups settings*,
+	//  and now you're in real hot water: parsing /etc files and hoping
+	//  nobody expects nsswitch to be too interesting.  Yeah.  Nuh uh.
+	//  (All of these are edge conditions tools like docker Don't Have because
+	//  they simply launch you with so much privilege that it doesn't matter.)
+
+	return nil
+}
+
+func buildCmd(frm api.Formula, chrootPath fs.AbsolutePath) *exec.Cmd {
+	cmdName := frm.Action.Exec[0]
+	cmd := exec.Command(cmdName, frm.Action.Exec[1:]...)
+	// TODO port policy concepts
+	// userinfo := cradle.UserinfoForPolicy(f.Action.Policy)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot: chrootPath.String(),
+		// TODO port policy concepts
+		//Credential: &syscall.Credential{
+		//	Uid: uint32(userinfo.Uid),
+		//	Gid: uint32(userinfo.Gid),
+		//},
+	}
+	cmd.Dir = string(frm.Action.Cwd)
+	cmd.Env = envToSlice(frm.Action.Env)
+	// TODO IO proxy wiring
+	return cmd
+}
+
+func envToSlice(env map[string]string) []string {
+	rv := make([]string, len(env))
+	i := 0
+	for k, v := range env {
+		rv[i] = k + "=" + v
+		i++
+	}
+	return rv
 }
