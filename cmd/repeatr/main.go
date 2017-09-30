27e8b036ab58451f6229f46d7496a80634157120
Eric Myhre <hash@exultant.us>
2017-09-30 21:47:36 +0200

    cli: start outlining.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/repeatr/main.go
+++ cmd/repeatr/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"io"
+	"os"
+)
+
+func main() {
+	ctx := context.Background()
+	exitCode := Main(ctx, os.Args, os.Stdin, os.Stdout, os.Stderr)
+	os.Exit(int(exitCode))
+}
+
+func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) int {
+	return 100
+}
---
+++ cmd/repeatr/main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCLIParse(t *testing.T) {
+
+}
