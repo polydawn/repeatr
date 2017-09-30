dfb55d9ec1b9ca8937165ed51b6c011a0861dc14
Eric Myhre <hash@exultant.us>
2017-09-30 19:52:17 +0200

    executor: prepare for more DRY tests.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- executor/impl/chroot/chrootExecutor_test.go
+++ executor/impl/chroot/chrootExecutor_test.go
@@ -14,6 +14,24 @@ import (
 	"go.polydawn.net/rio/stitch"
 )
 
+// Base formula full of sensible defaults and ready to run:
+var baseFormula = api.Formula{
+	Inputs: map[api.AbsPath]api.WareID{
+		"/": api.WareID{"tar", "6q7G4hWr283FpTa5Lf8heVqw9t97b5VoMU6AGszuBYAz9EzQdeHVFAou7c4W9vFcQ6"},
+	},
+	Action: api.FormulaAction{
+		Exec: []string{"/bin/echo", "hello world"},
+	},
+	Outputs: map[api.AbsPath]api.OutputSpec{
+		"/": {PackType: "tar", Filters: api.Filter_NoMutation},
+	},
+	FetchUrls: map[api.AbsPath][]api.WarehouseAddr{
+		"/": []api.WarehouseAddr{
+			"file://../../../fixtures/busybash.tgz",
+		},
+	},
+}
+
 func TestChrootExecutor(t *testing.T) {
 	var (
 		unpackTool rio.UnpackFunc = rioexecclient.UnpackFunc
@@ -21,31 +39,18 @@ func TestChrootExecutor(t *testing.T) {
 	)
 
 	WithTmpdir(func(tmpDir fs.AbsolutePath) {
-		t.Run("hello-world formula should work", func(t *testing.T) {
-			frm := api.Formula{
-				Inputs: map[api.AbsPath]api.WareID{
-					"/": api.WareID{"tar", "6q7G4hWr283FpTa5Lf8heVqw9t97b5VoMU6AGszuBYAz9EzQdeHVFAou7c4W9vFcQ6"},
-				},
-				Action: api.FormulaAction{
-					Exec: []string{"/bin/echo", "hello world"},
-				},
-				Outputs: map[api.AbsPath]api.OutputSpec{
-					"/": {PackType: "tar", Filters: api.Filter_NoMutation},
-				},
-				FetchUrls: map[api.AbsPath][]api.WarehouseAddr{
-					"/": []api.WarehouseAddr{
-						"file://../../../fixtures/busybash.tgz",
-					},
-				},
-			}
+		// Setup assembler and executor.  Both are reusable.
+		asm, err := stitch.NewAssembler(unpackTool)
+		AssertNoError(t, err)
+		exe := Executor{
+			osfs.New(tmpDir.Join(fs.MustRelPath("ws"))),
+			asm,
+			packTool,
+		}
 
-			asm, err := stitch.NewAssembler(unpackTool)
-			AssertNoError(t, err)
-			exe := Executor{
-				osfs.New(tmpDir.Join(fs.MustRelPath("ws"))),
-				asm,
-				packTool,
-			}
+		t.Run("hello-world formula should work", func(t *testing.T) {
+			frm := baseFormula.Clone()
+
 			rr, err := exe.Run(context.Background(), frm, repeatr.InputControl{}, repeatr.Monitor{})
 			WantNoError(t, err)
 			t.Logf("%v\n", rr)
