7fe1f2883faa686d9f117b6b2ea96b28f9f9ebbe
Eric Myhre <hash@exultant.us>
2017-09-30 22:28:43 +0200

    cli: more args parsing, bindings, and test setup.
    I'm going for some fairly esoteric code style here, but the aims are:
    
    - get the struct defs to appear exactly once,
    - get the defs to *not go very far*,
    - keep the args binding setup calls *very* close to the relevant structs,
    - keep the func that turns the struct into a Real Stuff call close to both of them,
    - and be very, very testable.
    
    The first four are pretty hard given this library's API, to be honest.  But we seem to be doing pretty well here.
    
    The "be testable" part is also nontrival.  I'm trying something new where we return the parsed args before running anything, so we can write tests that inspect it.
    
    This object for test inspection is a little wild because A) it has no reason to exist in the real main code paths and B) its type safety is 0% but C) that *doesn't matter* because the main code *doesn't actually use it*; it's just hanging things on the struct so that they just *happen* to be visible to the test harness when it's the caller.
    
    The test code doesn't really end up with much of a way to inspect the structs with types, either, which is odd.  But I think with the level of vision we want here, chugging the things back through a json serializer and doing big stinky string diffs should be... fine, honestly.  (The alternative is that the struct definitions have to move up to top-level declarations, and that makes the other objectives about keeping the binding methods closeby much harder.)
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/repeatr/main.go
+++ cmd/repeatr/main.go
@@ -2,16 +2,64 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
+
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
 	ctx := context.Background()
-	exitCode := Main(ctx, os.Args, os.Stdin, os.Stdout, os.Stderr)
+	bhv := Main(ctx, os.Args, os.Stdin, os.Stdout, os.Stderr)
+	exitCode := bhv.action()
 	os.Exit(int(exitCode))
 }
 
-func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) int {
-	return 100
+// Holder type which makes it easier for us to inspect
+//  the args parser result in test code before running logic.
+type behavior struct {
+	parsedArgs interface{}
+	action     func() int
+}
+
+func Main(ctx context.Context, args []string, stdin io.Reader, stdout, stderr io.Writer) behavior {
+	// CLI boilerplate.
+	app := kingpin.New("repeatr", "Functional computation.")
+	app.HelpFlag.Short('h')
+	app.UsageWriter(stderr)
+	app.ErrorWriter(stderr)
+
+	// Args struct defs and flag declarations.
+	bhvs := map[string]behavior{}
+	argsRun := struct {
+		FormulaPath string
+	}{}
+	cmdRun := app.Command("run", "Execute a formula.")
+	cmdRun.Arg("formula", "Path to formula file.").
+		Required().
+		StringVar(&argsRun.FormulaPath)
+	bhvs[cmdRun.FullCommand()] = behavior{&argsRun,
+		func() int {
+			// TODO do stuff
+			return 100
+		},
+	}
+
+	// Parse!
+	parsedCmdStr, err := app.Parse(args[1:])
+	if err != nil {
+		return behavior{
+			parsedArgs: err,
+			action: func() int {
+				fmt.Fprintln(stderr, err)
+				return 1
+			},
+		}
+	}
+	// Return behavior named by the command and subcommand strings.
+	if bhv, ok := bhvs[parsedCmdStr]; ok {
+		return bhv
+	}
+	panic("unreachable, cli parser must error on unknown commands")
 }
--- cmd/repeatr/main_test.go
+++ cmd/repeatr/main_test.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"testing"
 )
 
-func TestCLIParse(t *testing.T) {
+// Returns the behavior from an invocation of Main.
+func determineBehavior(args ...string) behavior {
+	stdin := &bytes.Buffer{}
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	return Main(context.Background(), args, stdin, stdout, stderr)
+}
 
+func TestCLIParse(t *testing.T) {
+	bhv := determineBehavior("repeatr", "wow")
+	t.Logf("%#v\n", bhv.parsedArgs)
+
+	bhv = determineBehavior("repeatr", "run")
+	t.Logf("%#v\n", bhv.parsedArgs)
+
+	bhv = determineBehavior("repeatr", "run", "file.frm")
+	t.Logf("%#v\n", bhv.parsedArgs)
 }
