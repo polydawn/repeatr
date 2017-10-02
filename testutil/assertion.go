4fb508f3f0d6ae31b2b7c071269485364a4e1ac6
Eric Myhre <hash@exultant.us>
2017-10-02 13:13:25 +0200

    testutil: quietly admit all useage is backwards.
    Opinions seem to be mixed in other libraries about how this should go: 'stretchr/testify' does expected-then-actual; 'go-check/check' does actual-then-expected.
    
    I seem to be writing things in actual-then-expected order out of habit, so I guess I'm going to stick with that.  I can't come up with strong arguments either way with a straight face.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- testutil/assertion.go
+++ testutil/assertion.go
@@ -19,10 +19,10 @@ func lambdaNoError(act thunk, err error) {
 	}
 }
 
-func AssertEqual(t *testing.T, want, got interface{}) { t.Helper(); lambdaEqual(t.Fatalf, want, got) }
-func WantEqual(t *testing.T, want, got interface{})   { t.Helper(); lambdaEqual(t.Errorf, want, got) }
-func lambdaEqual(act thunk, want, got interface{}) {
-	if reflect.DeepEqual(want, got) == false {
+func AssertEqual(t *testing.T, got, want interface{}) { t.Helper(); lambdaEqual(t.Fatalf, got, want) }
+func WantEqual(t *testing.T, got, want interface{})   { t.Helper(); lambdaEqual(t.Errorf, got, want) }
+func lambdaEqual(act thunk, got, want interface{}) {
+	if reflect.DeepEqual(got, want) == false {
 		act("expected equality: want %v, got %v", want, got)
 	}
 }
