33a3618111ca0f3b2ec21b0b72c63388388df97d
Eric Myhre <hash@exultant.us>
2017-10-05 13:19:13 +0200

    example: tighten up, output, link bootstrap asset.
    We're hacking the file+ca warehouse warehouse mode with a symlink.  Kids, (probably) don't do this at home.  (But, this dir's layout *is* considered part of our contract...)
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- example_pipeline.sh
+++ example_pipeline.sh
@@ -5,6 +5,10 @@ rm -r .pipeline.demo || true
 mkdir .pipeline.demo
 cd .pipeline.demo
 
+mkdir wares
+mkdir -p wares/6q7/G4hW
+ln -s ../../../../fixtures/busybash.tgz wares/6q7/G4hW/6q7G4hWr283FpTa5Lf8heVqw9t97b5VoMU6AGszuBYAz9EzQdeHVFAou7c4W9vFcQ6
+
 hitch init
 hitch catalog create "demo.polydawn.net/pipeline/base"
 hitch release start "demo.polydawn.net/pipeline/base" "v0.1"
@@ -20,14 +24,14 @@ frm="$(cat <<EOF
 			},
 			"action": {
 				"exec": ["/bin/echo", "hello world!"]
+			},
+			"outputs": {
+				"/task/out": {"packtype": "tar"}
 			}
 		},
 		"context": {
-			"fetchUrls": {
-				"/": [
-					"file://./fixtures/busybash.tgz"
-				]
-			}
+			"fetchUrls": {"/": ["ca+file://./wares/"]},
+			"saveUrls": {"/task/out": "ca+file://./wares/"}
 		}
 	}
 EOF
