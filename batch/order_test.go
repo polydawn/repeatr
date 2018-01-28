9ae9d302f54db730a3fe6d206e31d082f4fde279
Eric Myhre <hash@exultant.us>
2018-01-28 17:59:55 +0100

    Additional graph tests.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- batch/order_test.go
+++ batch/order_test.go
@@ -97,3 +97,65 @@ func TestSimpleLinearOrdering(t *testing.T) {
 		"stepD",
 	})
 }
+
+func TestComplexOrdering(t *testing.T) {
+	/*
+		             /------------> K --\
+		             |                   \
+		A --> B -----E ------> H --------> L
+		            /                     /
+		  C --> D -----F --> G ----------/
+		               |
+		               \------> I----------> M
+		               |                    /
+		               \--------> J -------/
+	*/
+	basting := api.Basting{Steps: map[string]api.BastingStep{
+		"stepA": {},
+		"stepB": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepA", "/out"},
+		}},
+		"stepC": {},
+		"stepD": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepC", "/out"},
+		}},
+		"stepE": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/":  {"wire", "stepB", "/out"},
+			"/1": {"wire", "stepD", "/out"},
+		}},
+		"stepF": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepD", "/out"},
+		}},
+		"stepG": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepF", "/out"},
+		}},
+		"stepH": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepE", "/out"},
+		}},
+		"stepI": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepF", "/out"},
+		}},
+		"stepJ": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepF", "/out"},
+		}},
+		"stepK": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/": {"wire", "stepE", "/out"},
+		}},
+		"stepL": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/":  {"wire", "stepG", "/out"},
+			"/1": {"wire", "stepK", "/out"},
+			"/2": {"wire", "stepH", "/out"},
+		}},
+		"stepM": {Imports: map[api.AbsPath]api.ReleaseItemID{
+			"/":  {"wire", "stepI", "/out"},
+			"/1": {"wire", "stepJ", "/out"},
+		}},
+	}}
+	order, err := orderSteps(basting)
+	WantEqual(t, err, nil)
+	WantEqual(t, order, []string{
+		"stepA", "stepB", "stepC", "stepD", "stepE",
+		"stepF", "stepG", "stepH", "stepI", "stepJ",
+		"stepK", "stepL", "stepM",
+	})
+}
